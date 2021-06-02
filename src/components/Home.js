03088fbe1b7ca2a1e102904b6f113405a840e695
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-06-02 22:39:55 +0900

    re-commit components

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import '../App.css';
+import React from 'react';
+//import logo from '../logo.svg';
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h2>Home Page</h2>
+					<main>
+						<p>This section contains information about...</p>
+					</main>
+				</header>
+			</div>
+
+		);
+	}
+
+}
+
+export default Home;
